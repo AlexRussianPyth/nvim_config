a7d8f34a375a671013f03784dcbc77af9a65b43d
Alex <alexvkleschov@gmail.com>
2024-02-16 23:00:46 +0400

    small fixes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,23 +1,24 @@
 local map = vim.api.nvim_set_keymap
-local default_opts = {noremap = true, silent = true}
+local default_opts = { noremap = true, silent = true }
 
 -----------------------------------------------------------
 -- НАВИГАЦИЯ
 -----------------------------------------------------------
 -- Стрелочки отключаем
-map('', '<up>', ':echoe "Use hjkl, bro"<CR>', {noremap = true, silent = false})
-map('', '<down>', ':echoe "Use hjkl, bro"<CR>', {noremap = true, silent = false})
-map('', '<left>', ':echoe "Use hjkl, bro"<CR>', {noremap = true, silent = false})
-map('', '<right>', ':echoe "Use hjkl, bro"<CR>', {noremap = true, silent = false})
+map("", "<up>", ':echoe "Use hjkl, bro"<CR>', { noremap = true, silent = false })
+map("", "<down>", ':echoe "Use hjkl, bro"<CR>', { noremap = true, silent = false })
+map("", "<left>", ':echoe "Use hjkl, bro"<CR>', { noremap = true, silent = false })
+map("", "<right>", ':echoe "Use hjkl, bro"<CR>', { noremap = true, silent = false })
 
 -- <F5> разные вариации нумераций строк, можно переключаться
-map('n', '<F5>', ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)
+map("n", "<F5>", ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)
 -- Выходим в нормальный режим через <jk>, чтобы не тянуться
-map('i', 'jk', '<Esc>', {noremap = true})
-map('i', 'ол', '<Esc>', {noremap = true})
-
+map("i", "jk", "<Esc>", { noremap = true })
+map("i", "ол", "<Esc>", { noremap = true })
+-- Сохраняемся удобнее
+map("n", "<C-S>", ":update<CR>", {})
 -----------------------------------------------------------
 -- ПОИСК
 -----------------------------------------------------------
 -- Выключить подсветку поиска
-map('n', ',<space>', ':nohlsearch<CR>', {noremap = true})
+map("n", ",<space>", ":nohlsearch<CR>", { noremap = true })
